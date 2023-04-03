3be2c7e25213a48b0f5b2f8a63c2d6eb02013b47
Rachana Devi <rachanadevi18@gmail.com>
2023-04-03 21:26:16 +0530

    Add event onClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -11,7 +11,7 @@ function ListGroup() {
             <h1>List</h1>
             {items.length === 0 && <p>No result found</p>} {/*if first is true then the next element will be rendered*/}
             <ul className="list-group">
-                {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
+                {items.map((item, index) => <li key={item} className="list-group-item" onClick={(event) => console.log("CLICKED",event,  item, index)}>{item}</li>)}
             </ul>
         </>
     );
