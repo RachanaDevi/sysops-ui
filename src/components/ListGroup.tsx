89762fad2c6d0e5184438d2cb6629396370a7852
Rachana Devi <rachanadevi18@gmail.com>
2023-04-03 21:14:21 +0530

    Refactor by extracting constants

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,10 +6,12 @@ function ListGroup() {
         'London'
     ]
 
+    const message = items.length === 0 ? <p>No result found</p> : null
+
     return (
         <>  {/* Fragment by default */}
             <h1>List</h1>
-            {items.length === 0 ? <p>No result found</p> : null}
+            {message}
             <ul className="list-group">
                 {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
             </ul>
