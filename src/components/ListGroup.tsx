a271911206524d213df8762bcc957d5d3fd91fe6
Rachana Devi <rachanadevi18@gmail.com>
2023-04-03 21:16:43 +0530

    Render conditional using &&

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,12 +6,10 @@ function ListGroup() {
         'London'
     ]
 
-    const getMessage = () => items.length === 0 ? <p>No result found</p> : null
-
     return (
         <>  {/* Fragment by default */}
             <h1>List</h1>
-            {getMessage()}
+            {items.length === 0 && <p>No result found</p>} {/*if first is true then the next element will be rendered*/}
             <ul className="list-group">
                 {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
             </ul>
