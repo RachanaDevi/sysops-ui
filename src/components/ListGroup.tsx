2d826af69596194be382f9dffa786a00601e37e5
Rachana Devi <rachanadevi18@gmail.com>
2023-04-03 21:35:36 +0530

    Use state to select the selected element from the list group

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
     let items = [
         'NewYork',
@@ -5,13 +7,18 @@ function ListGroup() {
         'Tokyo',
         'London'
     ]
+    // Hook is a function to tap into built in functions in react
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    // arr[0] // variable selectedIndex
+    // arr[1] // updater function 
 
     return (
         <>  {/* Fragment by default */}
             <h1>List</h1>
             {items.length === 0 && <p>No result found</p>} {/*if first is true then the next element will be rendered*/}
             <ul className="list-group">
-                {items.map((item, index) => <li key={item} className="list-group-item" onClick={(event) => console.log("CLICKED",event,  item, index)}>{item}</li>)}
+                {items.map((item, index) => <li key={item} className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} onClick={() => { setSelectedIndex(index); }}>{item}</li>)}
             </ul>
         </>
     );
