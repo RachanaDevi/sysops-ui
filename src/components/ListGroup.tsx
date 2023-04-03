3901caa2464308c3d6e34a4b3d6663971a6ec584
Rachana Devi <rachanadevi18@gmail.com>
2023-04-03 21:14:49 +0530

    Refactor by extracting method

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,12 +6,12 @@ function ListGroup() {
         'London'
     ]
 
-    const message = items.length === 0 ? <p>No result found</p> : null
+    const getMessage = () => items.length === 0 ? <p>No result found</p> : null
 
     return (
         <>  {/* Fragment by default */}
             <h1>List</h1>
-            {message}
+            {getMessage()}
             <ul className="list-group">
                 {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
             </ul>
